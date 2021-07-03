b5656af714a359ec67d7afdeec8ea04da25685ea
James <jameslilley97@gmail.com>
2021-07-04 00:35:41 +0100

    added comments to work and neater line spacing

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,10 @@
-#my script needs to convert the second column to an integer / float and calculate the average
-#my script needs to find the largest and smallest number on a given row
-#my script needs to figure out the number of months on the sheet, by counting the number of unqiue rows
-
-#consider assigning values to dict, searching dict for value then returning date? 
-
 import csv
 import os
 
 storedvalues = []
 
+#retrieve values for sum, count rows to get months
 budget_csv = os.path.join("Resources", "budget_data.csv")
-
-
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
@@ -20,12 +13,14 @@ with open(budget_csv, 'r') as csvfile:
         storedvalues.append(int(row[1]))
     months = i + 1
 
+#use list to get relevant values/indexes
 maxrow = storedvalues.index(max(storedvalues))+1
 minrow = storedvalues.index(min(storedvalues))+1
 maxval = max(storedvalues)
 minval = min(storedvalues)
 average = sum(storedvalues) / months
 
+#go back through list to get the dates of max / min
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     for i, row in enumerate(csvreader):
@@ -34,14 +29,15 @@ with open(budget_csv, 'r') as csvfile:
         elif i == minrow:
             mindate = row[0]
 
+    #text for messages
     titleoutput = (f"Financial Analysis \n----------------------------\n")
     totaloutput = (f"Total Months: {months}\nTotal: ${sum(storedvalues)}\n")    
     averageoutput = (f"Average Change: ${round(average, 2)}\n")
     minmaxoutput = (f"Greatest Increase In Profits: {maxdate} (${maxval})\nGreatest Deacrease In Profits: {mindate} (${minval})")
     output = titleoutput + totaloutput + averageoutput + averageoutput + minmaxoutput 
 
+#create txt file, print/send output
 output_path = os.path.join(".", "Analysis", "pybankreport.txt")
 with open(output_path, 'w') as txtfile:
     txtfile.write(output)
     print(output)
-    
