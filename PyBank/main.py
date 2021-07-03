22d0f9c33c19f5b62d9e7e5c0f74ac9cbc77ee6a
James <jameslilley97@gmail.com>
2021-07-03 17:17:53 +0100

    the code returns all values in sheet

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,16 @@
-print("Hello World!")
+#my script needs to convert the second column to an integer / float and calculate the average
+#my script needs to find the largest and smallest number on a given row
+#my script needs to figure out the number of months on the sheet, by counting the number of unqiue rows
+
+import csv
+import os
+
+budget_csv = os.path.join("Resources", "budget_data.csv")
+
+# Read in the CSV file
+with open(budget_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for row in csvreader:
+        print(row)
