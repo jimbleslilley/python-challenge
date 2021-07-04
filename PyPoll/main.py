17e3ce6a4670e359b5a528f12eb8a82d028b3d05
James <jameslilley97@gmail.com>
2021-07-04 02:59:40 +0100

    need to add way to count frequency of candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,7 +1,9 @@
 import csv
 import os
+import collections
 
-votebox = set(())
+votelist = []
+candidates = set(())
 
 poll_csv = os.path.join("Resources", "election_data.csv")
 with open(poll_csv, 'r') as csvfile:
@@ -9,9 +11,21 @@ with open(poll_csv, 'r') as csvfile:
     header = next(csvreader)
 
     for i, row in enumerate(csvreader):
-        votebox.add(row[2])
+        candidates.add(row[2])
+        votelist.append([row[2]])
+        
+totalvotes = (len(votelist))
 
-print(votebox)
+# https://www.kite.com/python/answers/how-to-count-item-frequency-in-python#:~:text=Use%20a%20for%2Dloop%20to,value%20to%20which%20item%20maps.
+tally = {}
+votetuple = tuple(votelist)
+for item in votelist:
+    if item in tally:
+        tally[item] += 1
+    else:
+        tally[item] = 1
+
+print(tally)
 #Output_path = os.path.join(".", "Analysis", "pybankreport.txt")
 #with open(output_path, 'w') as txtfile:
     #txtfile.write(output)
