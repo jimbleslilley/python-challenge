58c3fc53950378586dac09c1f41518c525a3e290
James <jameslilley97@gmail.com>
2021-07-04 01:48:22 +0100

    adds unique vote candidates to set and prints set

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+import csv
+import os
+
+votebox = set(())
+
+poll_csv = os.path.join("Resources", "election_data.csv")
+with open(poll_csv, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    for i, row in enumerate(csvreader):
+        votebox.add(row[2])
+
+print(votebox)
+#Output_path = os.path.join(".", "Analysis", "pybankreport.txt")
+#with open(output_path, 'w') as txtfile:
+    #txtfile.write(output)
+    #print(output)
