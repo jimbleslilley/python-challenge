14a92a3e391cd15b1eff41e5b818606d5a98a0be
James <jameslilley97@gmail.com>
2021-07-05 03:38:41 +0100

    printing and writing to file, correctly calculating numbers

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import os
 
 votebox = []
 candidates = set(())
-tally = dict()
+results = dict()
 y = 0
 
 #access csv 
@@ -12,30 +12,40 @@ with open(poll_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
-    #add each vote to votebox, add unique candidate to candidates
+    #add each vote to votebox, add unique candidate to candidates, store total votes
     for i, row in enumerate(csvreader): 
         candidates.add(row[2])
         votebox.append([row[2]])
 
+totalvotes = (len(votebox))
 
 #check each list and count each candidates votes, store in dict
 for candidate in candidates:
     for vote in votebox:
         if candidate == ' '.join(vote):
             y += 1
-    tally.update({candidate:y})
+    results.update({candidate:y})
     y = 0
 
-#https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value
-placing = {k: v for k, v in sorted(tally.items(), key=lambda item: item[1])}
+#https://stackoverflow.com/questions/613183/how-do-i-sort-a-dictionary-by-value, sorts the results dictionary by value ascending, instead of by key
+tally = {k: v for k, v in sorted(results.items(), key=lambda item: item[1])}
 
-totalvotes = (len(votebox))
-print(placing)
-print(totalvotes)
+#take the names from the ascending dictionary and put them in reverse order (so the list is ordered from first to last place for the upcoming loop)
+tallylist = list(tally)
+tallylist.reverse()
 
+#header and footers
+header = (f"Election Results\n-------------------------\nTotal Votes: {totalvotes}\n-------------------------")
+footer = (f"-------------------------\nWinner: {tallylist[0]}\n-------------------------")
 
-
-#Output_path = os.path.join(".", "Analysis", "pybankreport.txt")
-#with open(output_path, 'w') as txtfile:
-    #txtfile.write(output)
-    #print(output)
+#loop through sorted list, calculate percentage, print and write file to document
+output_path = os.path.join(".", "Analysis", "pypollreport.txt")
+with open(output_path, 'w') as txtfile:
+    print(header)
+    print(header, file = txtfile)
+    for candidate in tallylist:
+        average = "{:.3f}".format((tally.get(candidate) / totalvotes) * 100)
+        print(f"{candidate}: {average}% ({tally.get(candidate)})")
+        print(f"{candidate}: {average}% ({tally.get(candidate)})", file = txtfile)
+    print(footer)
+    print(footer, file = txtfile)
