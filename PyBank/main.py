e8f9ede2bb48fb7094b11bfb0a806f6b0849be18
James <jameslilley97@gmail.com>
2021-07-05 04:40:29 +0100

    massive overhaul, only reads file once to get relevant information, calculates average properly, gets correct max and min changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,49 +2,43 @@ import csv
 import os
 
 storedvalues = []
+dates = []
 monthlychange = []
 
-#retrieve values for sum, count rows to get months
+#
 budget_csv = os.path.join("Resources", "budget_data.csv")
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
 
+    #loop through csv, put each section into separate lists
     for i, row in enumerate(csvreader):
         storedvalues.append(int(row[1]))
-    months = i + 1
+        dates.append(row[0])
 
-#use list to get relevant values/indexes
-maxrow = storedvalues.index(max(storedvalues))+1
-minrow = storedvalues.index(min(storedvalues))+1
-maxval = max(storedvalues)
-minval = min(storedvalues)
-
-#average calculations and defined
+#loop through stored results to record monthly changes to third list
 d = 0
 h = 1
-while h < len(storedvalues):
+while h <= len(storedvalues)-1:
     monthlychange.append(storedvalues[h] - storedvalues[d])
     d += 1
-    h += 1  
+    h += 1
 
+months = len(dates)
 average = sum(monthlychange) / len(monthlychange)
+maxval = max(monthlychange)
+minval = min(monthlychange)
+maxrow = monthlychange.index(max(monthlychange))+1
+minrow = monthlychange.index(min(monthlychange))+1
+maxdate = dates[maxrow]
+mindate = dates[minrow]
 
-#go back through list to get the dates of max / min
-with open(budget_csv, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    for i, row in enumerate(csvreader):
-        if i == maxrow:
-            maxdate = row[0]
-        elif i == minrow:
-            mindate = row[0]
-
-    #text for messages
-    titleoutput = (f"Financial Analysis \n----------------------------\n")
-    totaloutput = (f"Total Months: {months}\nTotal: ${sum(storedvalues)}\n")    
-    averageoutput = (f"Average Change: ${round(average, 2)}\n")
-    minmaxoutput = (f"Greatest Increase In Profits: {maxdate} (${maxval})\nGreatest Deacrease In Profits: {mindate} (${minval})")
-    output = titleoutput + totaloutput + averageoutput + minmaxoutput 
+#text for messages
+titleoutput = (f"Financial Analysis \n----------------------------\n")
+totaloutput = (f"Total Months: {months}\nTotal: ${sum(storedvalues)}\n")    
+averageoutput = (f"Average Change: ${round(average, 2)}\n")
+minmaxoutput = (f"Greatest Increase In Profits: {maxdate} (${maxval})\nGreatest Deacrease In Profits: {mindate} (${minval})")
+output = titleoutput + totaloutput + averageoutput + minmaxoutput 
 
 #create txt file, print/send output
 output_path = os.path.join(".", "Analysis", "pybankreport.txt")
